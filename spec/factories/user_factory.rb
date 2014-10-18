230fe5500a29c906bcb528bdc8f1d14f00deab16
Vladislav K. Synkov <vladsynkov@gmail.com>
2014-10-18 13:07:29 +0400

    Fix tests

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user, aliases: [:creator] do
-    sequence(:name) { |n| "profile #{n}" }
+    sequence(:name) { |n| "user #{n}" }
     sequence(:email) { |n| "person#{n}@example.com" }
     password '123456789'
   end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -74,7 +74,7 @@ describe Room do
   end
 
   context 'visit' do
-    let(:profiles) { game.creator }
+    let(:user) { game.creator }
 
     context 'before' do
       it 'should not be visited' do
