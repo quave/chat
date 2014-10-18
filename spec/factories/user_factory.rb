2b46354f6f5b7cb81c0db074d7009c69138d8733
Vladislav K. Synkov <vladsynkov@gmail.com>
2014-10-18 13:05:49 +0400

    Fix tests

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :profiles, aliases: [:creator] do
+  factory :user, aliases: [:creator] do
     sequence(:name) { |n| "profile #{n}" }
     sequence(:email) { |n| "person#{n}@example.com" }
     password '123456789'
