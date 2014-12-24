8d13ed7b7dbf1efe288d254fab149be480c00529
Quave <vladsynkov@gmail.com>
2014-12-24 14:18:57 +0300

    integration tests fix

--- spec/features/auth/auth_helper.rb
+++ spec/features/auth/auth_helper.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 include Devise::TestHelpers
 include Warden::Test::Helpers
 
-RSpec.configure do |config|
-  config.before(:suite) do
-    Warden.test_mode!
-    user = FactoryGirl.create(:user)
-    login_as(user, :scope => :user)
-  end
+unless defined? user
+  Warden.test_mode!
+  user = FactoryGirl.build(:user)
+  user = User.find_or_create_by({ email: user.email })
+  login_as(user, :scope => :user)
+end
 
+RSpec.configure do |config|
   config.after(:each) do
     Warden.test_reset!
   end
