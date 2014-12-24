51069724e9e1a4b22b1fda196c0fb3f2f8064b4c
Quave <vladsynkov@gmail.com>
2014-12-24 16:31:41 +0300

    integration tests

--- spec/features/auth/authed_spec.rb
+++ spec/features/auth/authed_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
-RSpec.shared_context 'authed' do
+RSpec.shared_examples 'authed' do
   before (:each) do
     user = FactoryGirl.create(:user)
     login_as user, :scope => :user, :run_callbacks => false
   end
 
+  scenario 'Visit main page' do
+    visit root_path
+    expect(page).to have_text 'Выход'
+  end
 
 end
